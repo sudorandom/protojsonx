96b31c660ef4426b2875a45587c9072a720a0d94
Kevin McDonald <kevin@sudorandom.com>
2026-06-21 19:17:41 +0200

    chore: add to the fuzz tests

--- fuzz_test.go
+++ fuzz_test.go
@@ -28,6 +28,21 @@ func FuzzUnmarshal(f *testing.F) {
 	f.Add([]byte(`{"stringField":null,"repeatedString":null,"mapStringString":null}`))
 	f.Add([]byte(`{"repeatedMessage":[null,{"name":"after nil","value":7}]}`))
 	f.Add([]byte(`{"stringField":"ok"} trailing`))
+	f.Add([]byte(`{"doubleField":-1.25e+10,"floatField":6.02e-23,"int64Field":"-9007199254740991","uint64Field":"18446744073709551615","bytesField":"AAECAwQ="}`))
+	f.Add([]byte(`{"doubleField":01}`))
+	f.Add([]byte(`{"floatField":1e,"int32Field":+12}`))
+	f.Add([]byte(`{"uint32Field":-1,"uint64Field":"-1"}`))
+	f.Add([]byte(`{"mapStringString":{"":"empty","quote\"key":"slash\\value","unicode\u20ac":"\ud83d\ude00","line\nkey":"tab\tvalue"}}`))
+	f.Add([]byte(`{"repeatedString":["","alpha","quote\"","slash\\","unicode\u20ac","\ud83d\ude00"],"mapStringString":{"a":"1","b":"2"}}`))
+	f.Add([]byte(`{"childField":{"name":"first","value":1},"repeatedMessage":[{"name":"a","value":1},null,{"name":"b","value":-2},{"name":"","value":0}]}`))
+	f.Add([]byte(`{"timestampField":"0001-01-01T00:00:00Z","durationField":"315576000000.999999999s"}`))
+	f.Add([]byte(`{"timestampField":"9999-12-31T23:59:59.999999999Z","durationField":"-315576000000.999999999s"}`))
+	f.Add([]byte(`{"timestampField":"not-a-time","durationField":"1.0000000000s"}`))
+	f.Add([]byte(`{"unknown":[{"deep":{"arr":[true,false,null,{"n":-12.34e-5}]}}],"childField":{"name":"kept","value":3}}`))
+	f.Add([]byte(`{"childField":{"name":"x","name":"duplicate"},"repeatedMessage":[{"value":1,"value":2}]}`))
+	f.Add([]byte(`{"doubleField":1,"double_field":2,"int64Field":"3","int64_field":"4"}`))
+	f.Add([]byte(`{"stringField":null,"childField":{"name":null,"value":null},"repeatedMessage":null,"mapStringString":{}}`))
+	f.Add([]byte(`{"repeatedString":[null],"mapStringString":{"k":null},"repeatedMessage":[{"name":null}]}`))
 
 	f.Fuzz(func(t *testing.T, data []byte) {
 		var msg testpb.ComplexMessage
